dd7d2e3d491be41daecb2ab60530d0e2e00c6abd
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2011-09-06 11:10:58 +0200

    Spawn working (at least on one node).

--- src/spawn_test.erl
+++ src/spawn_test.erl
@@ -1,25 +1,37 @@
+%% @author Alessandro Sivieri <sivieri@elet.polimi.it>
+%% @doc Test module for remote spawn capabilities of WSN-Erlang framework.
 -module(spawn_test).
 -export([start/1, start/2, launch/1, test/0]).
 
+%% @doc Start the simulation with the given topology.
+%% @spec start(string()) -> [{ok, reference()}|{error, string()}]
+-spec(start(string()) -> [{ok, reference()}|{error, string()}]).
 start(Filename) ->
     Net = wsn:read_net(Filename),
     wsn:spawn_net(Net, ?MODULE, test).
 
+%% @doc Start the simulation with the given topology on the given hosts.
+%% @spec start(string(), [atom()]) -> [{ok, reference()}|{error, string()}]
 start(Filename, Hosts) ->
     Net = wsn:read_net(Filename),
     wsn:spawn_net(Net, Hosts, ?MODULE, test).
 
+%% @doc Start the spawning process from the given node.
+%% @spec launch(atom()) -> ok
+-spec(launch(atom()) -> ok).
 launch(DestId) ->
     wsn:send_ignore_gain(get(myid), DestId, go),
     ok.
 
+%% @private
+-spec(test() -> none()).
 test() ->
     receive
         go ->
-            wsn:spawn(get(myid), all, fun() -> io:format("~p~n", [get(myid)]) end),
+            wsn:spawn(get(myid), all, fun() -> io:format("~p~n", [node()]) end),
             test();
         Any ->
-            io:format("~p: waiting for messages...", [get(myid)]),
-            Any,
+            io:format("~p: received ~p~n", [get(myid), Any]),
+            io:format("~p: waiting for messages...~n", [get(myid)]),
             test()
     end.
