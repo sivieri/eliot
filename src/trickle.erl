d5c0a87d07b76130bbec02cb5d6579c229bfcf80
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2011-09-13 12:30:54 +0200

    Moved trickle to binary things.

--- src/trickle.erl
+++ src/trickle.erl
@@ -1,11 +1,12 @@
 %% @author Alessandro Sivieri <sivieri@elet.polimi.it>
 %% @reference <a href="http://portal.acm.org/citation.cfm?id=1251177">Trickle</a>
 -module(trickle).
--export([start/1, start_simulation/1, start_simulation/2, trickle/0, update_version/2]).
+-export([start/1, start_simulation/1, start_simulation/2, trickle/0, update_version/3]).
 -define(TAU_MAX, 60000).
 -define(TAU_MIN, 1000).
 -define(K, 2).
--define(PAYLOAD_STRING, "Payload version ").
+-define(SRCADDR, 16/unsigned-little-integer).
+-define(VERSION, 32/unsigned-little-integer).
 
 % Public API
 
@@ -38,65 +39,48 @@ trickle() ->
     T = utils:random(Tau),
     TRef = erlang:send_after(T, self(), transmit),
     TauRef = erlang:send_after(Tau, self(), restart),
-    trickle(Tau, {TauRef, TRef}, 0, {1, string(1)}).
+    Id = get(myaddr),
+    Msg = <<Id:?SRCADDR, 0:?VERSION, <<0>>/binary>>,
+    trickle(Tau, {TauRef, TRef}, 0, Msg).
 
 %% @doc Update the version of the given node.
-%% @spec update_version(atom(), integer()) -> ok
--spec(update_version(atom(), integer()) -> ok).
-update_version(DestId, Version) ->
-    wsn:send_ignore_gain(console, DestId, {update, Version, string(Version)}),
+%% @spec update_version(atom(), integer(), any()) -> ok
+-spec(update_version(atom(), integer(), any()) -> ok).
+update_version(DestId, Version, Payload) ->
+    Id = utils:nodeaddr(DestId),
+    PayloadB = term_to_binary(Payload),
+    Msg = <<Id:?SRCADDR, Version:?VERSION, PayloadB/binary>>,
+    wsn:send_ignore_gain(console, DestId, {version, Msg}),
 	ok.
 
 % Private API
 
 %% @private
--spec(trickle(non_neg_integer(), {reference(), reference()}, non_neg_integer(), {integer(), string()}) -> none()).
-trickle(Tau, {TauRef, TRef}, Counter, {Version, Payload}) ->
+-spec(trickle(non_neg_integer(), {reference(), reference()}, non_neg_integer(), binary()) -> none()).
+trickle(Tau, {TauRef, TRef}, Counter, <<Src:?SRCADDR, Version:?VERSION, Payload/binary>>) ->
     receive
         transmit when Counter < ?K ->
-            wsn:send(get(myid), all, {version, Version}),
-            trickle(Tau, {TauRef, TRef}, Counter, {Version, Payload});
+            Id = get(myaddr),
+            wsn:send(get(myid), all, {version, <<Id:?SRCADDR, Version:?VERSION, Payload/binary>>}),
+            trickle(Tau, {TauRef, TRef}, Counter, <<Src:?SRCADDR, Version:?VERSION, Payload/binary>>);
         transmit ->
-            trickle(Tau, {TauRef, TRef}, Counter, {Version, Payload});
+            trickle(Tau, {TauRef, TRef}, Counter, <<Src:?SRCADDR, Version:?VERSION, Payload/binary>>);
         restart ->
             NewTau = utils:update_tau(Tau, ?TAU_MIN, ?TAU_MAX),
             T = utils:random(NewTau),
             NewTRef = erlang:send_after(T, self(), transmit),
             NewTauRef = erlang:send_after(NewTau, self(), restart),
-            trickle(NewTau, {NewTauRef, NewTRef}, 0, {Version, Payload});
-        {_SourceId, _RSSI, {version, NewVersion}} when NewVersion < Version ->
-            %io:format("~p: Received version ~p from ~p with RSSI = ~p (older)~n", [get(myid), NewVersion, SourceId, RSSI]),
-            wsn:send(get(myid), all, {update, Version, Payload}),
-            erlang:cancel_timer(TRef),
-            trickle(Tau, {TauRef, TRef}, Counter, {Version, Payload});
-        {SourceId, RSSI, {version, NewVersion}} when NewVersion > Version ->
+            trickle(NewTau, {NewTauRef, NewTRef}, 0, <<Src:?SRCADDR, Version:?VERSION, Payload/binary>>);
+        {SourceId, RSSI, {version, <<NewSrc:?SRCADDR, NewVersion:?VERSION, NewPayload/binary>>}} when NewVersion > Version ->
             io:format("~p: Received version ~p from ~p with RSSI = ~p (newer)~n", [get(myid), NewVersion, SourceId, RSSI]),
-            wsn:send(get(myid), all, {version, Version}),
             erlang:cancel_timer(TRef),
             erlang:cancel_timer(TauRef),
             NewTau = ?TAU_MIN,
             T = utils:random(NewTau),
             NewTRef = erlang:send_after(T, self(), transmit),
             NewTauRef = erlang:send_after(NewTau, self(), restart),
-            trickle(NewTau, {NewTauRef, NewTRef}, 0, {Version, Payload});
-        {_SourceId, _RSSI, {version, NewVersion}} when NewVersion == Version ->
-            %io:format("~p: Received version ~p from ~p with RSSI = ~p (same)~n", [get(myid), NewVersion, SourceId, RSSI]),
-            trickle(Tau, {TauRef, TRef}, Counter + 1, {Version, Payload});
-        {SourceId, RSSI, {update, NewVersion, NewPayload}} when NewVersion > Version ->
-            io:format("~p: Received code ~p from ~p with RSSI = ~p (newer)~n", [get(myid), NewVersion, SourceId, RSSI]),
-            erlang:cancel_timer(TRef),
-            erlang:cancel_timer(TauRef),
-            NewTau = ?TAU_MIN,
-            T = utils:random(NewTau),
-            NewTRef = erlang:send_after(T, self(), transmit),
-            NewTauRef = erlang:send_after(NewTau, self(), restart),
-            trickle(NewTau, {NewTauRef, NewTRef}, 0, {NewVersion, NewPayload});
-        {_SourceId, _RSSI, {version, _NewVersion, _NewPayload}} ->
+            trickle(NewTau, {NewTauRef, NewTRef}, 0, <<NewSrc:?SRCADDR, NewVersion:?VERSION, NewPayload/binary>>);
+        {_SourceId, _RSSI, {version, <<_NewSrc:?SRCADDR, _NewVersion:?VERSION, _NewPayload/binary>>}} ->
             %io:format("~p: Received code ~p from ~p with RSSI = ~p (same or older)~n", [get(myid), NewVersion, SourceId, RSSI]),
-            trickle(Tau, {TauRef, TRef}, Counter, {Version, Payload})
+            trickle(Tau, {TauRef, TRef}, Counter, <<Src:?SRCADDR, Version:?VERSION, Payload/binary>>)
     end.
-
-%% @private
--spec(string(integer()) -> string()).
-string(Version) ->
-    ?PAYLOAD_STRING ++ integer_to_list(Version).
