10df4b5e6ef2cc4964f1237b49fff6a861208616
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2012-01-04 12:31:30 +0100

    Compiled Rebar with previous version.

---
+++ examples/trickle/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 qualified_name"
+    exit 1
+fi
+
+cd `dirname $0`
+exec erl +P 1000000 -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -name wsn@$1 -setcookie abc -s trickle
