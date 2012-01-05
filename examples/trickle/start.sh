d67bdcecc5868061f6297150ba1fd3beb9725a5b
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2012-01-05 16:35:43 +0100

    Changed LinkLayerModel and shell scripts.

--- examples/trickle/start.sh
+++ examples/trickle/start.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 qualified_name node_index"
+if [ $# -eq 2 ]; then
+    cd `dirname $0`
+    exec erl +P 1000000 -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -name wsn@$1 -setcookie abc -s trickle start $2 false
+elif [ $# -eq 3 ]; then
+    cd `dirname $0`
+    exec erl +P 1000000 -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -name wsn@$2 -setcookie abc -s trickle start $3 $1
+else
+    echo "Usage: $0 qualified_name node_index | $0 config_file, qualified_name node_index"
     exit 1
 fi
-
-cd `dirname $0`
-exec erl +P 1000000 -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -name wsn@$1 -setcookie abc -s trickle start $2 false
---
+++ priv/templates/wsnapp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ $# -eq 2 ]; then
+    cd `dirname $0`
+    exec erl +P 1000000 -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -name wsn@$1 -setcookie abc -s {{appid}} start $2 false
+elif [ $# -eq 3 ]; then
+    cd `dirname $0`
+    exec erl +P 1000000 -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -name wsn@$2 -setcookie abc -s {{appid}} start $3 $1
+else
+    echo "Usage: $0 qualified_name node_index | $0 config_file, qualified_name node_index"
+    exit 1
+fi
