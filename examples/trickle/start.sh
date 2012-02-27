317fc127bff902a161bed19ca9de123901b61dbd
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2012-02-27 13:10:42 +0100

    Working scripts and makefiles.

--- examples/trickle/start.sh
+++ examples/trickle/start.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-if [ $# -eq 2 ]; then
-    cd `dirname $0`
-    exec erl +P 1000000 -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -name eliot@$1 -setcookie abc -s trickle start $2 false
-elif [ $# -eq 3 ]; then
-    cd `dirname $0`
-    exec erl +P 1000000 -pa $PWD/simbin $PWD/deps/*/ebin -boot start_sasl -name eliot@$2 -setcookie abc -s trickle start $1
+if [ $# -eq 3 ]; then
+    if [ $1 = "simulate" ]; then
+        cd `dirname $0`
+        exec erl +P 1000000 -pa $PWD/simbin $PWD/deps/*/simbin -boot start_sasl -name eliot@$2 -setcookie abc -s trickle start $3
+    else
+        cd `dirname $0`
+        exec erl +P 1000000 -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -name eliot@$2 -setcookie abc -s trickle start $3
+    fi
 else
-    echo "Usage: $0 qualified_name node_index | $0 config_file qualified_name node_index"
+    echo "Usage: $0 run qualified_name node_index | $0 simulate qualified_name config_file"
     exit 1
 fi
