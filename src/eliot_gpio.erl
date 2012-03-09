ac60e55adeef17602697def0c272bf0ca8964afe
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2012-03-09 11:58:08 +0100

    Corrected gpio functions.h

--- src/eliot_gpio.erl
+++ src/eliot_gpio.erl
@@ -2,7 +2,7 @@
 %% @doc GPIO interface.
 -module(eliot_gpio).
 -behaviour(gen_server).
--export([start_link/0, dirin/1, dirout/1, get/1, set/2, clear/1]).
+-export([start_link/0, dirin/1, dirout/1, get/1, set/2]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -record(state, {port}).
 
@@ -23,27 +23,18 @@ get(Pin) ->
 set(Pin, Value) ->
     gen_server:cast(?MODULE, {set, Pin, Value}).
 
-clear(Pin) ->
-    gen_server:cast(?MODULE, {clear, Pin}).
-
 init([]) ->
     {ok, #state{port = create_port()}}.
 
 handle_call({get, Pin}, _From, #state{port = Port} = State) ->
-    Port ! {self(), {command, term_to_binary({get, Pin})}},
-    receive
-        {Port, {data, Data}} ->
-            {reply, binary_to_term(Data), State}
-    end.
+    Res = send_int(Port, {get, Pin}),
+    {reply, Res, State}.
 
 handle_cast({direction, Direction}, #state{port = Port} = State) ->
-    Port ! {self(), {command, term_to_binary(Direction)}},
+    send_int(Port, Direction),
     {noreply, State};
 handle_cast({set, Pin, Value}, #state{port = Port} = State) ->
-    Port ! {self(), {command, term_to_binary({set, Pin, Value})}},
-    {noreply, State};
-handle_cast({clear, Pin}, #state{port = Port} = State) ->
-    Port ! {self(), {command, term_to_binary({clear, Pin})}},
+    send_int(Port, {set, Pin, Value}),
     {noreply, State}.
 
 handle_info({Port, {exit_status, Status}}, #state{port = Port} = State) ->
@@ -65,4 +56,18 @@ create_port() ->
             exit(error);
         PrivDir ->
             open_port({spawn, filename:join([PrivDir, "eliot_gpio"])}, [binary, {packet, 4}, exit_status])
+    end.
+
+send_int(Port, Msg) ->
+    Port ! {self(), {command, term_to_binary(Msg)}},
+    receive
+        {Port, {data, Data}} ->
+            Res = binary_to_term(Data),
+            case Res of
+                {error, Error} ->
+                    io:format("GPIO error: ~p~n", [Error]);
+                _ ->
+                    ok
+            end,
+            Res
     end.
