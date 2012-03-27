d9211b67c527e92559876874ad741e097f6265e3
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2012-03-27 16:45:14 +0200

    Solved a bug.

--- priv/templates/eliotapp.erl
+++ priv/templates/eliotapp.erl
@@ -7,7 +7,7 @@
 start(Param) ->
     application:set_env(eliot, simulation, true),
     application:start(eliot),
-    wsn_simulator:start({{appid}}_task, Param).
+    eliot_simulator:start({{appid}}_task, Param).
 -else.
 start(Param) ->
     eliot_api:set_node_name(Param),
