ff7743386c01d3a2427ac9a2695e9b1019a6be41
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2012-08-27 16:54:40 +0200

    New test values.

--- examples/appliance/src/appliances.erl
+++ examples/appliance/src/appliances.erl
@@ -6,17 +6,17 @@
 
 ac(Id) ->
     application:set_env(appliance, type, ac),
-    application:set_env(appliance, params, [#parameter{name = temperature, type = temperature}, #parameter{name = starttime, type = time}, #parameter{name = endtime, type = time}]),
+    application:set_env(appliance, params, [#parameter{name = temperature, type = temperature, value = 25, fixed = true}, #parameter{name = starttime, type = time}, #parameter{name = endtime, type = time, value = 6}]),
     appliance:start(Id).
 
 dw(Id) ->
     application:set_env(appliance, type, dw),
-    application:set_env(appliance, params, [#parameter{name = starttime, type = time}, #parameter{name = endtime, type = time}]),
+    application:set_env(appliance, params, [#parameter{name = starttime, type = time}, #parameter{name = endtime, type = time, value = 1}]),
     appliance:start(Id).
 
 wm(Id) ->
     application:set_env(appliance, type, wm),
-    application:set_env(appliance, params, [#parameter{name = starttime, type = time}, #parameter{name = endtime, type = time}]),
+    application:set_env(appliance, params, [#parameter{name = starttime, type = time}, #parameter{name = endtime, type = time, value = 1}]),
     appliance:start(Id).
 
 % Private API
