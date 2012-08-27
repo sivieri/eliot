7f9bc827de8eb0b655b4bdf0538353d2ec0e3d7b
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2012-08-27 12:56:24 +0200

    More appliances.

--- examples/appliance/src/appliances.erl
+++ examples/appliance/src/appliances.erl
@@ -1,14 +1,21 @@
 -module(appliances).
--export([ac/1, dishwasher/1]).
+-export([ac/1, dw/1, wm/1]).
 -include("scenario.hrl").
 
 % Public API
 
 ac(Id) ->
+    application:set_env(appliance, type, ac),
     application:set_env(appliance, params, [#parameter{name = temperature, type = temperature}, #parameter{name = starttime, type = time}, #parameter{name = endtime, type = time}]),
     appliance:start(Id).
 
-dishwasher(Id) ->
+dw(Id) ->
+    application:set_env(appliance, type, dw),
+    application:set_env(appliance, params, [#parameter{name = starttime, type = time}, #parameter{name = endtime, type = time}]),
+    appliance:start(Id).
+
+wm(Id) ->
+    application:set_env(appliance, type, wm),
     application:set_env(appliance, params, [#parameter{name = starttime, type = time}, #parameter{name = endtime, type = time}]),
     appliance:start(Id).
 
