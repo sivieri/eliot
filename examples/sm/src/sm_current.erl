14855579f9359098587f2dd1b6fe312464e4f8e7
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2013-01-16 12:11:13 +0100

    Corrections/2.

--- examples/sm/src/sm_current.erl
+++ examples/sm/src/sm_current.erl
@@ -1,6 +1,7 @@
 -module(sm_current).
 -export([start_link/1, current/1]).
 -include("eliot.hrl").
+-include("scenario.hrl").
 -define(TIMER, 60 * 1000).
 -record(state, {company = none}).
 
@@ -17,7 +18,7 @@ current(#state{company = Company} = State) ->
     receive
         send ->
             Dest = utils:join_name(?NODENAME, Company),
-            {company, Dest} ~ eliot_api:msg(term_to_binary({sm, current, 100})),
+            {company, Dest} ~ eliot_api:msg(<<?CONSUMPTION:8/unsigned-little-integer, 100:16/unsigned-little-integer>>),
             erlang:send_after(?TIMER, self(), send);
         Any ->
             io:format("Sm Current: Unknown message ~p~n", [Any])
