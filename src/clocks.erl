23d38fef715c65989e89c788f9b3ecd523664638
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2013-02-06 17:28:05 +0100

    Corrections.

--- src/clocks.erl
+++ src/clocks.erl
@@ -6,15 +6,34 @@
 
 % Public API
 
-start(Type) ->
-    Val = unixtime:Type(),
-    #stopwatch{type = Type, start = Val}.
+start(times) ->
+    Val = unixtime:times(),
+    #stopwatch{type = times, start = Val};
+start(clock) ->
+    Val = unixtime:clock(),
+    #stopwatch{type = clock, start = Val};
+start(gettimeofday) ->
+    {Secs, USecs} = unixtime:gettimeofday(),
+    Val = Secs * 1000000 + USecs,
+    #stopwatch{type = gettimeofday, start = Val};
+start(clock_gettime) ->
+    {Secs, NSecs} = unixtime:clock_gettime(),
+    Val = Secs * 1000000000 + NSecs,
+    #stopwatch{type = clock_gettime, start = Val}.
 
 update(#stopwatch{type = clock, start = Start} = SW) ->
     Val = unixtime:clock(),
     SW#stopwatch{cur = Val - Start};
-update(#stopwatch{type = Type, start = {S1, S2}} = SW) ->
-    {Cur1, Cur2} = unixtime:Type(),
-    SW#stopwatch{cur = {Cur1 - S1, Cur2 - S2}}.
+update(#stopwatch{type = times, start = {S1, S2}} = SW) ->
+    {Cur1, Cur2} = unixtime:times(),
+    SW#stopwatch{cur = {Cur1 - S1, Cur2 - S2}};
+update(#stopwatch{type = gettimeofday, start = Start} = SW) ->
+    {Secs, USecs} = unixtime:gettimeofday(),
+    Val = Secs * 1000000 + USecs,
+    SW#stopwatch{cur = Val - Start};
+update(#stopwatch{type = clock_gettime, start = Start} = SW) ->
+    {Secs, NSecs} = unixtime:clock_gettime(),
+    Val = Secs * 1000000000 + NSecs,
+    SW#stopwatch{cur = Val - Start}.
 
 % Private API
