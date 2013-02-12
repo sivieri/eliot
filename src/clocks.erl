b37b5c48cf20ff19ad06999d04769ad56ddccb9e
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2013-02-12 18:04:47 +0100

    Correction3.

--- src/clocks.erl
+++ src/clocks.erl
@@ -36,20 +36,34 @@ update(#stopwatch{type = clock_gettime, start = Start} = SW) ->
     Val = Secs * 1000000000 + NSecs,
     SW#stopwatch{cur = Val - Start}.
 
-acc_start(#stopwatch{type = times} = SW) ->
+acc_start(#stopwatch{type = times, acc = none} = SW) ->
     Val = unixtime:times(),
     SW#stopwatch{startacc = Val, acc = {0, 0}};
+acc_start(#stopwatch{type = times} = SW) ->
+    Val = unixtime:times(),
+    SW#stopwatch{startacc = Val};
+acc_start(#stopwatch{type = clock, acc = none} = SW) ->
+    Val = unixtime:clock(),
+    SW#stopwatch{startacc = Val, acc = 0};
 acc_start(#stopwatch{type = clock} = SW) ->
     Val = unixtime:clock(),
+    SW#stopwatch{startacc = Val};
+acc_start(#stopwatch{type = gettimeofday, acc = none} = SW) ->
+    {Secs, USecs} = unixtime:gettimeofday(),
+    Val = Secs * 1000000 + USecs,
     SW#stopwatch{startacc = Val, acc = 0};
 acc_start(#stopwatch{type = gettimeofday} = SW) ->
     {Secs, USecs} = unixtime:gettimeofday(),
     Val = Secs * 1000000 + USecs,
+    SW#stopwatch{startacc = Val};
+acc_start(#stopwatch{type = clock_gettime, acc = none} = SW) ->
+    {Secs, NSecs} = unixtime:clock_gettime(),
+    Val = Secs * 1000000000 + NSecs,
     SW#stopwatch{startacc = Val, acc = 0};
 acc_start(#stopwatch{type = clock_gettime} = SW) ->
     {Secs, NSecs} = unixtime:clock_gettime(),
     Val = Secs * 1000000000 + NSecs,
-    SW#stopwatch{startacc = Val, acc = 0}.
+    SW#stopwatch{startacc = Val}.
 
 acc_stop(#stopwatch{type = clock, startacc = Start, acc = Acc} = SW) ->
     Val = unixtime:clock(),
