fc732832ce30285f243b9c2c7493fb3832fbeb08
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2013-02-20 11:13:24 +0100

    Bug/2.

--- examples/sm/src/sm_task.erl
+++ examples/sm/src/sm_task.erl
@@ -85,7 +85,7 @@ sm(#state{company = Company, appliances = Appliances, slots = Slots, cap = Cap, 
             SW2 = clocks:acc_stop(SW),
             {ok, Dev} = application:get_env(sm, logger),
             io:format(Dev, "SCHED~c~p~n", [9, SW2#stopwatch.last]),
-            sm(#state{company = Company, appliances = Schedule});
+            sm(#state{company = Company, appliances = Schedule, sw = SW2});
         {_RSSI, Source, Content} ->
             {NewCompany, NewAppliances, NewSlots, NewCap} = case Content of
                 <<?COMPANY:8/unsigned-little-integer, CCap:16/unsigned-little-integer, Other/binary>> when Company == none ->
