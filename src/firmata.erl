4afed940760a4b0ce20d60db725d873ee57817e8
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2013-10-18 19:41:57 +0200

    Firmata working; renamed serial library.

--- src/firmata.erl
+++ src/firmata.erl
@@ -16,11 +16,15 @@ digital_read(Pin) ->
 analog_read(Pin) ->
     gen_server:call(?MODULE, {analog, read, Pin}).
 
-pin_mode(Pin, Mode) ->
-    gen_server:cast(?MODULE, {mode, Pin, Mode}).
+pin_mode(Pin, input) ->
+    gen_server:cast(?MODULE, {mode, Pin, 0});
+pin_mode(Pin, output) ->
+    gen_server:cast(?MODULE, {mode, Pin, 1}).
 
-digital_write(Pin, Value) ->
-    gen_server:cast(?MODULE, {digital, write, Pin, Value}).
+digital_write(Pin, low) ->
+    gen_server:cast(?MODULE, {digital, write, Pin, 0});
+digital_write(Pin, high) ->
+    gen_server:cast(?MODULE, {digital, write, Pin, 1}).
 
 analog_write(Pin, Value) ->
     gen_server:cast(?MODULE, {analog, write, Pin, Value}).
@@ -47,33 +51,37 @@ handle_cast({pin, Pin, Mode}, State = #state{device = Device}) ->
     Device ! {send, <<244:8/integer, Pin:8/integer, Mode:8/integer>>},
     {noreply, State};
 handle_cast({digital, write, Pin, Value}, State = #state{device = Device, output = Output}) ->
-    PortNumber = (pin bsr 3) band 15,
+    PortNumber = (Pin bsr 3) band 15,
     Current = dict:fetch(PortNumber, Output),
     NewCurrent = case Value of
         0 ->
             Current band (bnot (1 bsl (Pin band 7)));
         1 ->
-            Current bor (bnot (1 bsl (Pin band 7)))
+            Current bor (1 bsl (Pin band 7))
     end,
     NewOutput = dict:store(PortNumber, NewCurrent, Output),
     Cmd = 144 bor (PortNumber),
     First = NewCurrent band 127,
     Second = NewCurrent bsr 7,
-    Device ! {send, <<Cmd:8/integer, First:8/integer, Second:8/integer>>},
+    Msg = <<Cmd:8/integer, First:8/integer, Second:8/integer>>,
+    Device ! {send, Msg},
     {noreply, State#state{output = NewOutput}};
 handle_cast({analog, write, Pin, Value}, State = #state{device = Device}) ->
     Cmd = 224 bor (Pin band 15),
     First = Value band 127,
     Second = Value bsr 7,
-    Device ! {send, <<Cmd:8/integer, First:8/integer, Second:8/integer>>},
+    Msg = <<Cmd:8/integer, First:8/integer, Second:8/integer>>,
+    Device ! {send, Msg},
     {noreply, State};
 handle_cast(Msg, State) ->
     io:format(standard_error, "Unknown CAST message ~p~n", [Msg]),
     {noreply, State}.
 
 handle_info({data, NewBytes}, State = #state{analog = Analog, digital = Digital, bytes = Bytes}) when byte_size(NewBytes) + byte_size(Bytes)  >= 3 ->
-    {NewAnalog, NewDigital} = parse_msg(<<Bytes/binary, NewBytes/binary>>, Analog, Digital),
-    {noreply, State#state{analog = NewAnalog, digital = NewDigital, bytes = <<>>}};
+    Parsed = binary:part(<<Bytes/binary, NewBytes/binary>>, 0, (byte_size(NewBytes) + byte_size(Bytes)) div 3 * 3),
+    Rest = binary:part(<<Bytes/binary, NewBytes/binary>>, byte_size(NewBytes) + byte_size(Bytes), -((byte_size(NewBytes) + byte_size(Bytes)) rem 3)),
+    {NewAnalog, NewDigital} = filter_msg(Parsed, Analog, Digital),
+    {noreply, State#state{analog = NewAnalog, digital = NewDigital, bytes = Rest}};
 handle_info({data, NewBytes}, State) ->
     {noreply, State#state{bytes = NewBytes}};
 handle_info(Info, State) ->
@@ -88,11 +96,17 @@ code_change(_OldVsn, State, _Extra) ->
 
 % Private API
 
-parse_msg(<<>>, Analog, Digital) ->
+filter_msg(<<>>, Analog, Digital) ->
     {Analog, Digital};
-parse_msg(<<224:8/integer, Ch:8/integer, Lsb:8/integer, Msb:8/integer, Other/binary>>, Analog, Digital) ->
-    parse_msg(Other, dict:store(Ch, (Msb bsl 7) + Lsb, Analog), Digital);
-parse_msg(<<144:8/integer, Ch:8/integer, Lsb:8/integer, Msb:8/integer, Other/binary>>, Analog, Digital) ->
-    parse_msg(Other, Analog, dict:store(Ch, (Msb bsl 7) + Lsb, Digital));
-parse_msg(<<_Unknown:8/integer, Other/binary>>, Analog, Digital) ->
-    parse_msg(Other, Analog, Digital).
+filter_msg(<<B1:8/integer, B2:8/integer, B3:8/integer, Rest/binary>>, Analog, Digital) ->
+    Cmd = B1 band 240,
+    Ch = B1 band 15,
+    {NewAnalog, NewDigital, Continuation} = case Cmd of
+        224 -> {parse_msg(Ch, B2, B3, Analog), Digital, Rest};
+        144 -> {Analog, parse_msg(Ch, B2, B3, Digital), Rest};
+        _ -> {Analog, Digital, <<B2:8/integer, B3:8/integer, Rest/binary>>}
+    end,
+    filter_msg(Continuation, NewAnalog, NewDigital).
+
+parse_msg(Ch, Lsb, Msb, Dict) ->
+    dict:store(Ch, (Msb bsl 7) + Lsb, Dict).
